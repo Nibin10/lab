cbe54918d7a1b1dd19423625d538540af97c9b5d
Nibin Raj R <90279793+Nibin10@users.noreply.github.com>
2021-11-27 11:48:32 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,13 +1,30 @@
 #include<stdio.h>
-int p[30],bt[30],wt[30],tat[30],n,tot_tat=0,tot_wt=0,FCFS_tat=0,FCFS_wt=0,i;
+int p[30],at[30],bt[30],ct[30],wt[30],tat[30],n,tot_tat=0,tot_wt=0,FCFS_tat=0,FCFS_wt=0,i;
 void main()
 {
-    printf("Enter the number of process :");
+    printf("Enter the number of process : ");
     scanf("%d",&n);
+    printf("\nEnter the arrival time of each process :\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&at[i]);
+    }
     printf("Enter the burst time of process \n");
     for(i=0;i<n;i++)
     {
         scanf("%d",&bt[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i==0)
+        {
+            ct[i]=bt[i];
+        }
+        else
+        {
+            ct[i]=ct[i-1]+bt[i];
+        }
+
     }
     printf("FSFS Algoritham \n");
     table(&FCFS_tat,&FCFS_wt);
@@ -20,28 +37,20 @@ void table(int *a,int *b)
 {
     for(i=0;i<n;i++)
     {
-        if(i==0)
-        {
-            tat[i]=bt[i];
-        }
-        else
-        {
-            tat[i]=tat[i-1]+bt[i];
+            tat[i]=ct[i]-at[i];
             tot_tat=tot_tat+tat[i];
-        }
     }
     *a=tot_tat;
-    wt[0]=0;
-    for(i=1;i<n;i++)
+    for(i=0;i<n;i++)
     {
-            wt[i]=wt[i-1]+bt[i-1];
+            wt[i]=tat[i]-bt[i];
             tot_wt=tot_wt+wt[i];
     }
     *b=tot_wt;
-    printf("Process \tBurst time\tTurn around time\tWaiting Time\n");
+    printf("Process \tArrival time \tBurst time\tCompletion time \tTurn around time\tWaiting Time\n");
     for(i=0;i<n;i++)
     {
-        printf("process[%d]\t\t%d\t\t%d\t\t%d\n",i,bt[i],tat[i],wt[i]);
+        printf("process[%d]\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\n",i,at[i],bt[i],ct[i],tat[i],wt[i]);
     }
 
 }
